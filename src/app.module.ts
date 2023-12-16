735d7d9dc04f9e7e87c98c99246012fc911fbe10
Dmytro Horbatenko <dm.gorbatenko@gmail.com>
2023-12-16 21:52:02 +0200

    feat: add users crud api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UserModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
